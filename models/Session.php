d5486cb380ec4e9bb76df9546764c07178b26d83
AndronovaYuliya <andron39933993@gmail.com>
2018-12-09 12:51:52 +0200

    Edit Session.php

--- models/Session.php
+++ models/Session.php
@@ -2,36 +2,32 @@
 
 class Session
 {
-    private static $cookieName='';
+    private static $cookieName='sid';
     private static $started=false;
-    private static $lifeTime=6000;
+    private static $lifeTime=86400;
 
-    //проверка на существование сессии
-    public static function isCreated()
+    public static function isCreated():bool
     {
-      return (!empty($_COOKIE[self::$cookieName]));
+        return (!empty($_COOKIE[self::$cookieName]));
     }
 
-    //старт сессии
+    /*
+    *@ Return void
+    */
     public static function startSession()
     {
-        if(!self::$started)
-        {
-            session_set_cookie_params(self::$lifeTime, '/', false, true);
-           // session_save_path(dirname($_SERVER['DOCUMENT_ROOT']).'/Shop/var/session');
-
-            if(!empty($_COOKIE[self::$cookieName])){
-                unset($_COOKIE[self::$cookieName]);
-            }
-            //session_name(self::$cookieName);
+        if(!self::$started) {
+            session_set_cookie_params(self::$lifeTime);
+            session_save_path(dirname($_SERVER['DOCUMENT_ROOT']) . '/Shop/var/session');
+            session_name(self::$cookieName);
             session_start();
-            self::set('remote_addr', $_SERVER['REMOTE_ADDR']);
-            self::set('user_agent', $_SERVER['HTTP_USER_AGENT']);
-            self::$started=true;
+            self::$started = true;
         }
     }
 
-    //установка параметров сессии
+    /*
+     *@ Return void
+     */
     public static function set($name, $value)
     {
         if(self::$started)
@@ -40,17 +36,19 @@ class Session
         }
     }
 
-    //получение параметров сессии
-    public static function get($name):?string
+    /*
+     * @ Return Mixed
+     */
+    public static function get($name)
     {
         if(self::$started)
         {
-            return isset($_SESSION[$name])?$_SESSION[$name]:null;
+            return $_SESSION[$name];
         }
+        return null;
     }
 
 
-    //удаление параметров сессии
     public static function delete($name)
     {
         if(self::$started)
@@ -59,22 +57,46 @@ class Session
         }
     }
 
-    //удаление сессии
-    public static function destroySession()
+    /*
+   *@ Return bool
+   */
+    public static function destroySession():bool
     {
         if(self::$started)
         {
-            unset($_COOKIE[self::$cookieName]);
-            setcookie(self::$cookieName,'',1, '/');
+            setcookie(self::$cookieName,'',time()-1, '/');
             session_destroy();
+            unset($_SESSION);
             self::$started=false;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+   *@ Return bool
+   */
+    public static function checkCookie():bool
+    {
+        if(!empty($_COOKIE[self::$cookieName]))
+        {
+            if($_SESSION['user_agent']==$_SERVER['HTTP_USER_AGENT'] && $_SESSION['remote_addr']==$_SERVER['REMOTE_ADDR'])
+            {
+                return true;
+            }
+            else
+            {
+                self::destroySession();
+                return false;
+            }
         }
     }
 
-    //проверка на кражу cookie
-    private static function checkCookie():bool
+    public static function getSession():array
     {
-       return ($_SESSION['ua']==$_SERVER['HTTP_USER_AGENT'] && $_SESSION['remote_addr']==$_SERVER['REMOTE_ADDR']);
+        if(self::$started)
+        {
+            return $_SESSION;
+        }
     }
-
 }
