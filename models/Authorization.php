9660d4cbccb279a441590788de5ac28dcb953767
AndronovaYuliya <andron39933993@gmail.com>
2018-12-09 12:52:23 +0200

    Edit Authorization.php

--- models/Authorization.php
+++ models/Authorization.php
@@ -2,9 +2,10 @@
 
 class Authorization
 {
-    protected static $login = 'admin';
-    protected static $password = '$2y$10$rDpsG13U3NrTAyhr2s4HfugXK8lHrZx/hDS.IVdHFDTgW3TChaOgC';//admin
-    protected static $logged = false;
+    private static $login = 'admin';
+    private static $password = '$2y$10$rDpsG13U3NrTAyhr2s4HfugXK8lHrZx/hDS.IVdHFDTgW3TChaOgC';//admin
+    private static $logged = false;
+    private static $user = null;
 
     /**
      * @return string $login
@@ -24,20 +25,35 @@ class Authorization
 
     /**
      * @return bool
-     * сверка данных
      */
-    public static function auth($login, $password):bool
+    public static function login($login, $password):bool
     {
-       return ($login==self::$login && password_verify($password,self::$password));
+        if($login==self::$login && password_verify($password,self::$password))
+        {
+            self::$logged=true;
+            self::$user=self::$login;
+            Session::startSession();
+            self::addToSession();
+            return true;
+        }
+        return false;
     }
 
     /**
      * @return bool
-     * Проверка авторизации
      */
     public static function isAuth():bool
     {
-        return self::$logged;
+        Session::startSession();
+        $data=Session::get(self::$login);
+
+        if (!empty($data) && Session::checkCookie())
+        {
+            self::$logged=true;
+            session_decode($data);
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -45,14 +61,11 @@ class Authorization
      */
     public static function logout():bool
     {
-        Session::destroySession();
-        header('Location:  ../index.php');
-        exit();
+        return Session::destroySession();
     }
 
     /*
      * @return bool
-     * Валидация логина
     */
     public static function checkLogin($login):bool
     {
@@ -61,10 +74,19 @@ class Authorization
 
     /*
      * @return bool
-     * Валидация пароля
     */
     public static function checkPass($pass):bool
     {
         return true;
+    }
+
+    /*
+     * @void
+    */
+    private static function addToSession()
+    {
+        Session::set('remote_addr', $_SERVER['REMOTE_ADDR']);
+        Session::set('user_agent', $_SERVER['HTTP_USER_AGENT']);
+        Session::set(self::$login, session_encode());
     }
 }
